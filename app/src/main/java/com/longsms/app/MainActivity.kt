8d0b8375096ee4950c2956be2a81b2e12a535eb8
reza347 <83227675+reza347@users.noreply.github.com>
2026-08-29 12:09:22 +0330

    Change height of Surface from 70dp to 62dp

--- app/src/main/java/com/longsms/app/MainActivity.kt
+++ app/src/main/java/com/longsms/app/MainActivity.kt
@@ -699,7 +699,7 @@ private fun SendScreen(
                 Surface(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(70.dp),
+                        .height(62.dp),
                     shape = RoundedCornerShape(20.dp),
                     color = MaterialTheme.colorScheme.surface,
                     shadowElevation = 3.dp
